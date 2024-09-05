2377eecfe1596be485bf6cd7a198375dd7bfca1f
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-09-05 20:26:27 +0800

    Create 51. N 皇后.cpp

---
+++ 51. N 皇后.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool row[9];
+bool col[9];
+bool dgn[17];//y-x+9  
+bool dgn1[17];//x+y
+int gn;
+vector<vector<string>>ret;
+vector<string>path;
+string st;
+void dfs(int y)
+{
+	if (y != 0 && row[y-1] == false)
+	{
+		return;
+	}
+	if (y == gn)
+	{
+		ret.emplace_back(path);
+		return;
+	}
+	string st;
+	for (int x=0;x<gn;x++)
+	{
+		if (col[x] == false && dgn[y-x+8]==false&&dgn1[y+x]==false)
+		{
+			st += "Q";
+			col[x] = true;
+			dgn[y - x + 8] = true;
+			dgn1[y + x] = true;
+			row[y] = true;
+			int xt = x;
+			while (xt < gn-1)
+			{
+				xt++;
+				st += ".";
+			}
+			path.emplace_back(st);
+			dfs(y+1);
+			st[x] = '.';
+			while (xt > x)
+			{
+				xt--;
+				st.pop_back();
+			}
+			path.pop_back();
+			col[x] = false;
+			dgn[y - x + 8] = false;
+			dgn1[y + x] = false;
+			row[x] = false;
+		}
+		else
+		{
+			st += ".";
+		}
+	}
+}
+vector<vector<string>> solveNQueens(int n) {
+	gn = n;
+	dfs(0);
+	return ret;
+}
+};
