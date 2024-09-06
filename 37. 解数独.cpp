9002155608c08620fed34651d1d7297c43381eb5
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-09-06 17:40:18 +0800

    Update 37. 解数独.cpp

--- 37. 解数独.cpp
+++ 37. 解数独.cpp
@@ -1,63 +1,30 @@
 class Solution {
 public:
-bool row[9][9];
+   bool row[9][9];
 bool col[9][9];
 bool blk[3][3][9];
 bool vb[9][9];
 int m;
 int n;
 bool ret;
-void dfs(int x,int y,vector<vector<char>>&board)
+void dfs(int x, int y, vector<vector<char>>& board)
 {
 	if (x == 9)
 	{
 		ret = true;
 		return;
 	}
-	/*for (int i = x; i < m; i++)
+	if (vb[x][y] == false)
 	{
-		for (int j = y; j < n; j++)
-		{*/
-	int i = x;
-	int j = y;
-			if (vb[i][j] == false)
+		for (int k = 0; k < 9; k++)
+		{
+			if (row[x][k] == false && col[y][k] == false && blk[x / 3][y / 3][k] == false)
 			{
-				for (int k = 0; k < 9; k++)
+				row[x][k] = col[y][k] = blk[x / 3][y / 3][k] = vb[x][y] = true;
+				board[x][y] = k + '1';
+				if (y < 8)
 				{
-					if (row[i][k] == false && col[j][k] == false && blk[i / 3][j / 3][k] == false)
-					{
-						row[i][k] = col[j][k] = blk[i / 3][j / 3][k] = vb[i][j] = true;
-						board[i][j] = k + '1';
-						if (j < 8)
-						{
-							dfs(i, j+1,board);
-							if (ret == 1)
-							{
-								return;
-							}
-						}
-						else
-						{
-							dfs(i+1, 0, board);
-							if (ret == 1)
-							{
-								return;
-							}
-						}
-						board[i][j] = '.';
-						row[i][k] = col[j][k] = blk[i / 3][j / 3][k] = vb[i][j] = false;
-					}
-				}
-				if (board[i][j] == '.')
-				{
-					return;
-				}
-			}
-			else
-			{
-				if (j < 8)
-				{
-					dfs(i, j + 1, board);
+					dfs(x, y + 1, board);
 					if (ret == 1)
 					{
 						return;
@@ -65,13 +32,40 @@ void dfs(int x,int y,vector<vector<char>>&board)
 				}
 				else
 				{
-					dfs(i + 1, 0, board);
+					dfs(x + 1, 0, board);
 					if (ret == 1)
 					{
 						return;
 					}
 				}
+				board[x][y] = '.';
+				row[x][k] = col[y][k] = blk[x / 3][y / 3][k] = vb[x][y] = false;
 			}
+		}
+		if (board[x][y] == '.')
+		{
+			return;
+		}
+	}
+	else
+	{
+		if (y < 8)
+		{
+			dfs(x, y + 1, board);
+			if (ret == 1)
+			{
+				return;
+			}
+		}
+		else
+		{
+			dfs(x + 1, 0, board);
+			if (ret == 1)
+			{
+				return;
+			}
+		}
+	}
 }
 void solveSudoku(vector<vector<char>>& board) {
 	m = board.size();
@@ -82,8 +76,8 @@ void solveSudoku(vector<vector<char>>& board) {
 		{
 			if (board[i][j] != '.')
 			{
-				row[i][board[i][j]-'1'] = true;
-				col[j][board[i][j]-'1'] = true;
+				row[i][board[i][j] - '1'] = true;
+				col[j][board[i][j] - '1'] = true;
 				blk[i / 3][j / 3][board[i][j] - '1'] = true;
 				vb[i][j] = true;
 			}
